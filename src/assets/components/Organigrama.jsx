677408a10506159291af498d50dbcb777bc5ff4d
desarrolloTic2 <desarrollo.tic2.inverpacifico@gmail.com>
2023-12-29 17:08:56 -0500

    Add Bootstrap and Font Awesome dependencies, implement editing, deleting, and adding nodes in Organigrama component

--- src/assets/components/Organigrama.jsx
+++ src/assets/components/Organigrama.jsx
@@ -1,7 +1,8 @@
 import { Tree, TreeNode } from "react-organizational-chart";
+import { useState } from "react";
 
 function Organigrama() {
-  const tree = [
+  const [tree, setTree] = useState([
     {
       id: 1,
       name: "root",
@@ -22,21 +23,109 @@ function Organigrama() {
         { id: 5, name: "child3", children: [] },
       ],
     },
-  ];
+  ]);
+
+  const [editNodeId, setEditNodeId] = useState(null);
+
+  const generateUniqueId = () => {
+    const allIds = [];
+
+    const extractIds = (node) => {
+      allIds.push(node.id);
+      node.children.forEach((child) => extractIds(child));
+    };
+
+    tree.forEach((node) => extractIds(node));
+
+    const maxId = Math.max(...allIds);
+    return maxId + 1;
+  };
 
   const handleTreeClick = (node) => {
-    console.log("click", node);
-  }
+    setEditNodeId(node.id);
+  };
+  const handleSaveEdit = (editedName, nodeId) => {
+    const updateNode = (node) => {
+      if (node.id === nodeId) {
+        return { ...node, name: editedName };
+      }
+      const updatedChildren = node.children.map((childNode) =>
+        updateNode(childNode)
+      );
+      return { ...node, children: updatedChildren };
+    };
+
+    const updatedTree = tree.map((node) => updateNode(node));
+
+    setTree(updatedTree);
+    setEditNodeId(null);
+  };
+
+  const handleDeleteNode = (nodeId) => {
+    const removeNode = (node) => {
+      if (node.id === nodeId) {
+        return null;
+      }
+      const updatedChildren = node.children
+        .map((childNode) => removeNode(childNode))
+        .filter(Boolean);
+      return { ...node, children: updatedChildren };
+    };
+
+    const updatedTree = removeNode(tree[0]);
+    setTree([updatedTree]);
+  };
+
+  const handleAddNode = (parentId) => {
+    const newNodeId = generateUniqueId();
+    const newNode = { id: newNodeId, name: `nuevo${newNodeId}`, children: [] };
+
+    const addNode = (node) => {
+      if (node.id === parentId) {
+        return { ...node, children: [...node.children, newNode] };
+      }
+      const updatedChildren = node.children.map((childNode) =>
+        addNode(childNode)
+      );
+      return { ...node, children: updatedChildren };
+    };
+
+    const updatedTree = tree.map((node) => addNode(node));
+
+    setTree(updatedTree);
+  };
 
   const renderTree = (node) => (
     <TreeNode
       key={node.id}
       label={
-        <div
-          className="styleNode"
-          onClick={() =>handleTreeClick(node)}
-        >
-          {node.name}
+        <div className="styleNode">
+          <div className="mb-2" onClick={() => handleTreeClick(node)}>
+            {node.id === editNodeId ? (
+              <input
+                type="text"
+                defaultValue={node.name}
+                onBlur={(e) => handleSaveEdit(e.target.value, node.id)}
+              />
+            ) : (
+              node.name
+            )}
+          </div>
+          <button
+            className="btn btn-danger btn-sm me-1"
+            onClick={() => handleDeleteNode(node.id)}
+          >
+            <i
+              className="fas fa-trash "
+              onClick={() => handleDeleteNode(node.id)}
+            ></i>
+          </button>
+          <button
+            className="btn btn-primary btn-sm"
+            onClick={() => handleAddNode(node.id)}
+          >
+            <i className="fas fa-plus"></i>
+          </button>
         </div>
       }
     >
@@ -53,11 +142,24 @@ function Organigrama() {
         lineColor={"green"}
         lineBorderRadius={"2px"}
         label={
-          <div
-            className="styleNode"
-            onClick={() =>handleTreeClick(tree[0])}
-          >
-            {tree[0].name}
+          <div className="styleNode">
+            <div onClick={() => handleTreeClick(tree[0])}>
+              {tree[0].id === editNodeId ? (
+                <input
+                  type="text"
+                  defaultValue={tree[0].name}
+                  onBlur={(e) => handleSaveEdit(e.target.value, tree[0].id)}
+                />
+              ) : (
+                tree[0].name
+              )}
+            </div>
+            <button
+              className="btn btn-primary btn-sm"
+              onClick={() => handleAddNode(tree[0].id)}
+            >
+              <i className="fas fa-plus"></i>
+            </button>
           </div>
         }
       >
